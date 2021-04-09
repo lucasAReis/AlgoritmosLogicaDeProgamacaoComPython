358302d05d0f1135db078f4039e6593176f2868a
lucasAReis <ddstrapdonike@gmail.com>
2021-04-08 21:45:53 -0300

    calculadora

---
+++ terceiro_progama/Calculadora_finalizada.py
@@ -0,0 +1,21 @@
+Python 3.9.4 (tags/v3.9.4:1f2e308, Apr  6 2021, 13:40:21) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/leo/Desktop/AlgoritmosLogicaDeProgamacaoComPython/terceiro_progama/Calculadora.py
+>>> print. 2+2
+SyntaxError: invalid syntax
+>>> print 2+2
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(2+2)?
+>>> print ("2+2")
+2+2
+>>> >>> 2+3
+SyntaxError: invalid syntax
+>>> (*2+5)
+SyntaxError: can't use starred expression here
+>>> (2+5)
+7
+>>> (23+42)
+65
+>>> (5+9)
+14
+>>> 
