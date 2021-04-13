c07237c96dc7fdb5dd7e2734bcf328c97612e33c
lucasAReis <ddstrapdonike@gmail.com>
2021-04-12 21:09:38 -0300

    Lista_de_salarios_com_print

---
+++ sexto_programa/Calculo_de_aumento_de_salario.py
@@ -0,0 +1,17 @@
+print ("salario")
+
+
+print ("faxineira")
+
+
+print (1222 +34)
+
+print ("Mecanico")
+
+print (999 +777)
+
+print ("professora")
+
+print (2000 + 2000)
+
+print ("E assim que faz uma lista de pressos ")
